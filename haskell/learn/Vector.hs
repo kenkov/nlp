cbe5ae5e3df5ed735e5bf2e9853f0eda3704b940
Noriyuki Abe <kenko.py@gmail.com>
2014-09-14 17:17:08 +0900

    Implement l1svm

--- haskell/learn/Vector.hs
+++ haskell/learn/Vector.hs
@@ -3,6 +3,9 @@ module Vector (
     Dimension,
     innerProduct,
     l2Norm,
+    vectorSum,
+    scalaProduct,
+    sign,
 ) where
 
 type Vector = [Double]
@@ -11,5 +14,14 @@ type Dimension = Int
 innerProduct :: Vector -> Vector -> Double
 innerProduct x y = sum $ zipWith (*) x y
 
+vectorSum :: Vector -> Vector -> Vector
+vectorSum = zipWith (+)
+
+scalaProduct :: Double -> Vector -> Vector
+scalaProduct x = map (*x)
+
 l2Norm :: Vector -> Double
 l2Norm x = sum $ map (^2) x
+
+sign :: Double -> Double
+sign x = if x >= 0 then 1.0 else -1.0
