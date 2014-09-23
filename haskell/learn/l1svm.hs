56301f8dba7df0c0f5664311c4f4f92b48ad34a9
Noriyuki Abe <kenko.py@gmail.com>
2014-09-23 20:25:00 +0900

    Add {batch,online}l1svm

--- haskell/learn/l1svm.hs
+++ haskell/learn/l1svm.hs
@@ -1,49 +1,51 @@
 import Control.Applicative
 import Vector
+import System.IO
+import System.Environment
+import Data.List
+import SVM
 
-
-type Parameter = (Vector, Double)
 type Count = Int
-type LearningRate = Double
-
-data Point = Point {
-    point :: Vector,
-    value :: Double
-} deriving (Show, Eq)
 
 grad :: Dimension -> Double -> Parameter -> Point -> Parameter
 grad dim c (w, b) (Point x y) =
     (g1 |+| g2, b' + b'')
     where
-        disc = (y * (w |*| x)) + b < 1.0
+        disc = y * ((w |*| x) + b) < 1.0
         g1 = if disc
                 then scalaProduct (- y) x
                 else replicate dim 0
-        g2 = [if abs wi > 1.0e-15 then c * signum wi else 0 | wi <- x ]
+        g2 = [if abs wi > 1.0e-5 then c * signum wi else 0 | wi <- x ]
         b' = if disc then -y else 0
-        b'' = if abs b > 1.0e-15 then c * signum b else 0
+        b'' = if abs b > 1.0e-5 then c * signum b else 0
 
 learn1 :: LearningRate -> Dimension -> Double -> Parameter -> Point -> Parameter
 learn1 eta dim c (w, b) pt =
     let (gw, gb) = grad dim c (w, b)  pt in
         (zipWith (\ x y -> x - eta * y) w gw, b - eta * gb)
 
-learn :: LearningRate -> Dimension -> Double -> [Point] -> Parameter
-learn eta dim c = foldl (learn1 eta dim c) ([0, 0], 0)
+l1svm_ :: LearningRate -> Dimension -> Double -> [Point] -> Parameter -> Parameter
+l1svm_ eta dim c ps param = foldl (learn1 eta dim c) param ps
+
+rep :: (a -> a) -> a -> [b] -> a
+rep f x = foldl (\x y -> f x) x
+
+l1svm :: Count -> LearningRate -> Dimension -> Double -> [Point] -> Parameter
+l1svm count eta dim c ps = rep (l1svm_ eta dim c ps) (replicate dim 0, 0) [1..count]
+
+batchL1svm :: Count -> LearningRate -> Dimension -> Double -> [Point] -> Parameter
+batchL1svm = l1svm
+
+onlineL1svm :: LearningRate -> Dimension -> Double -> [Point] -> Parameter
+onlineL1svm = l1svm 1
 
 
 main :: IO ()
 main = do
-    let points = [Point [0, 0] (-1),
-                  Point [1, 0] (-1),
-                  Point [2, 0] (-1),
-                  Point [3, 0] (-1),
-                  Point [0, 1] 1,
-                  Point [1, 1] 1,
-                  Point [1, 2] 1,
-                  Point [1, 3] 1
-                  ]
+    points <- map (mkPoint . map read . words) . lines <$> getContents
+    let count = 100
     let eta = 0.1
     let dim = 2
-    let c = 0.1
-    print $ learn eta dim c points
+    let c = 1.0e-5
+    putStrLn $ showParameter $ batchL1svm count eta dim c points
+    -- putStrLn $ showParameter $ onlineL1svm eta dim c points
